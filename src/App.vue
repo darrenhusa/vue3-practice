bb7615a96f843416fee85f578d3a14bd901aaca1
Darren Henderson <darrenh@ccsj.edu>
2024-12-31 20:26:31 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TodoList from './components/TodoList.vue'
+</script>
+
+<template>
+  <div id ="app" class="container">
+      <TodoList />
+    </div>
+
+</template>
+
+<style>
+#app {
+  font-size: 24px;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
